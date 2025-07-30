1224a22378060f1007b2c1b6600d4e50e09a2c19
Aya de Raghũl <nguyenlinhchi09@gmail.com>
2025-07-30 22:00:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ app.use((err, req, res, next) => {
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Serve index.html on unmatched routes (for client-side routing)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// });
 
 app.use((req, res, next) => {
   console.log('🛑 No route matched:', req.method, req.originalUrl);
